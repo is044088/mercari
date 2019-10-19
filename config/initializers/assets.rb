168c2293a2aba871e5ec26528120a592c165e6ef
Hiroki Kawai <syojikisyukatsu@yahoo.co.jp>
2019-10-19 17:02:12 +0900

    precompiled toppage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( slick.css )
 Rails.application.config.assets.precompile += %w( slick-theme.css )
+Rails.application.config.assets.precompile += %w( ress.css )
+Rails.application.config.assets.precompile += %w( style.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
