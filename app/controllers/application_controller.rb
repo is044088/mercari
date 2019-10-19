9fb88b7793585a6aa9d6fa9929027e9c02f3f377
yasuna613 <yasuna.613@gmail.com>
2019-10-19 15:30:08 +0900

    add devise用controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
   private
 
@@ -12,5 +16,22 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :nickname,
+        :image_url,
+        :profile,
+        :family_name,
+        :first_name,
+        :ja_family_name,
+        :ja_first_name,
+        :birthday,
+        :authenticate_phone]
+    )  
   end
 end
