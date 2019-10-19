89e8c05ba46793725490824ee9e77a81a027fcfb
yasuna613 <yasuna.613@gmail.com>
2019-10-19 15:31:11 +0900

    logout画面用view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'items#index'
-  devise_for :users
+  devise_for :users, :controllers => {
+ :registrations => 'users/registrations',
+ :sessions => 'users/sessions'
+}
+  devise_scope :user do
+    get 'logout', to: 'users/sessions#logout', as: 'logout'
+  end
+
   resources :items
 end
