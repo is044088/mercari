0dd1bb31f0ccba0071da5066233f7bf8d9d4c1bb
yasuna613 <yasuna.613@gmail.com>
2019-10-20 17:03:49 +0900

    edit　サイドバー

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
+  get 'mypage' => 'users#show'
   get 'logout' => 'users#logout'
+  get 'mypage/profile' => 'users#profile'
+  get 'mypage/notification' => 'users#notification'
+  get 'mypage/todo' => 'users#todo'
+  get 'mypage/purchase' => 'users#purchase'
+  get 'mypage/purchased' => 'users#purchased'
   resources :items
 
 end
