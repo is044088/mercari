6948899bd7c6d20289877624449f807a2bd8c4a3
yasuna613 <yasuna.613@gmail.com>
2019-10-20 17:00:09 +0900

    add プロフィール編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,34 @@ class UsersController < ApplicationController
   def logout
   end
 
+  def update
+    if current_user.update(profile_params)
+      flash[:notice]= "変更しました"
+      redirect_to mypage_profile_path
+    else
+      render :profile
+    end
+  end
+
+  def profile
+
+  end
+
+  def notification
+  end
+
+  def todo
+  end
+
+  def purchase
+  end
+
+  def purchased
+  end
+
+  private
+  def profile_params
+    params.require(:user).permit(:nickname, :profile)
+  end
+
 end
