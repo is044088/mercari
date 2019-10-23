c9e2029f262a6083234b042c2e5a838a2cba59f9
renato-sugiyama <qx24.rs2357@gmail.com>
2019-10-23 20:52:59 +0900

    update_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
   root 'items#index'
-  devise_for :users, :controllers => {
- :registrations => 'users/registrations',
- :sessions => 'users/sessions'
-}
-  devise_scope :user do
-    get 'logout', to: 'users/sessions#logout', as: 'logout'
-  end
+  devise_for :users
 
-  get "items/index" => "items#index"
-  get "items/show" => "items#show"
-  get "items/delete" => "items#delete"
-  get "items/create" => "items#create"
+  resources :users
+  get 'mypage' => 'users#show'
+  get 'logout' => 'users#logout'
+  get 'mypage/profile' => 'users#profile'
+  get 'mypage/notification' => 'users#notification'
+  get 'mypage/todo' => 'users#todo'
+  get 'mypage/purchase' => 'users#purchase'
+  get 'mypage/purchased' => 'users#purchased'
+  resources :items
+
+  resources :items, only: [:new, :create] do
+    resources :images, only: :create
+    end
+    resources :items, only: [:index, :show, :new, :edit, :create, :update, :destroy] do
+      #Ajaxで動くアクションのルートを作成
+      collection do
+        get 'get_category_children', defaults: { format: 'json' }
+        get 'get_category_grandchildren', defaults: { format: 'json' }
+      end
+    end
 end
