502ce36f1fe5b43a4786b39f29264637cadc2cb9
yasuna613 <yasuna.613@gmail.com>
2019-10-26 16:53:57 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
-
   resources :users
-  get 'mypage' => 'users#show'
-  get 'logout' => 'users#logout'
-  get 'mypage/profile' => 'users#profile'
-  get 'mypage/notification' => 'users#notification'
-  get 'mypage/todo' => 'users#todo'
-  get 'mypage/purchase' => 'users#purchase'
-  get 'mypage/purchased' => 'users#purchased'
   get "items/index" => "items#index"
   get "items/show" => "items#show"
   get "items/delete" => "items#delete"
   resources :items
+
+  # マイページ
+  get 'logout' => 'users#logout'
+  get 'mypage' => 'users#show'
+  get 'profile' => 'mypage#profile'
+  get 'notification' =>'mypage#notification'
+  get 'todo' => 'mypage#todo'
+  get 'purchase' => 'mypage#purchase'
+  get 'purchased' => 'mypage#purchased'
+  get 'mydate' => 'mypage#mydate'
+  get 'authenticate_phone' =>'mypage#authenticate_phone'
+  get 'card' => 'mypage#card'
+  get 'deliver_address' =>'mypage#deliver_address'
 end
