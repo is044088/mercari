6901a77f71d01f1047c642ffdd3eeb00e085175b
marumaru77 <marumaru07unchain@gmail.com>
2019-11-15 00:46:39 +0900

    validates

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,9 @@
 class SignupController < ApplicationController
+  before_action :validates_step1, only: :step2 
+  before_action :validates_step2, only: :step3 
+  before_action :validates_step3, only: :step4
+  before_action :validates_step4, only: :step5
+
 
   def step0
   end
@@ -8,34 +13,15 @@ class SignupController < ApplicationController
   end
 
   def step2
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:first_name] = user_params[:first_name]
-    session[:family_name] = user_params[:family_name]
-    session[:ja_first_name] = user_params[:ja_first_name]
-    session[:ja_family_name] = user_params[:ja_family_name]
-    session['birthday(1i)'] = params[:user]['birthday(1i)'].to_i
-    session['birthday(2i)'] = params[:user]['birthday(2i)'].to_i
-    session['birthday(3i)'] = params[:user]['birthday(3i)'].to_i
     @user = User.new
   end
 
   def step3
-    session[:authenticate_phone] = user_params[:authenticate_phone]
     @user = User.new
     @user.build_address
   end
 
   def step4
-    # session[:postal_code] = user_params[:address_attributes]['postal_code']
-    # session[:prefecture] = user_params[:address_attributes]['prefecture']
-    # session[:city] = user_params[:address_attributes]['city']
-    # session[:street_number] = user_params[:address_attributes]['street_number']
-    # session[:building_name] = user_params[:address_attributes]['building_name']
-    # session[:delivery_phone] = user_params[:address_attributes]['delivery_phone']
-    session[:address] = user_params[:address_attributes]
     @user = User.new
     @user.build_card
   end
@@ -55,27 +41,107 @@ class SignupController < ApplicationController
       image_url: "default_image.jp",
       birthday: birthday_date
     )
-    # @address = Address.new(
-    #   postal_code: session[:postal_code],
-    #   prefecture: session[:prefecture],
-    #   city: session[:city],
-    #   street_number: session[:street_number],
-    #   building_name: session[:building_name],
-    #   delivery_phone: session[:delivery_phone]
-    # )
     @user.build_address(session[:address])
     @user.build_card(user_params[:card_attributes])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to("/")
+      redirect_to("/signup/done")
     else
       render step4_signup_index_path 
+    end 
+  end
+  
+  def done
+    sign_in User.find(session[:user_id]) unless user_signed_in?
+  end
+  
+#バリデーションーーーーーーーーーーーーーーーーーー
+
+  def validates_step1 
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:first_name] = user_params[:first_name]
+    session[:family_name] = user_params[:family_name]
+    session[:ja_first_name] = user_params[:ja_first_name]
+    session[:ja_family_name] = user_params[:ja_family_name]
+    session['birthday(1i)'] = params[:user]['birthday(1i)'].to_i
+    session['birthday(2i)'] = params[:user]['birthday(2i)'].to_i
+    session['birthday(3i)'] = params[:user]['birthday(3i)'].to_i
+    # date = Date.new session['birthday(1i)'].to_i,session['birthday(2i)'].to_i,session['birthday(3i)'].to_i
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      first_name: session[:first_name],
+      family_name: session[:family_name],
+      ja_first_name: session[:ja_first_name],
+      ja_family_name: session[:ja_family_name],
+      birthday: 00000000,
+      authenticate_phone: '00000000'
+    )
+    unless @user.valid?(:validates_step1)
+      flash[:notice] = @user.errors.full_messages 
+      render '/signup/step1' 
     end
-    
   end
 
-  
-  
+  def validates_step2
+    session[:authenticate_phone] = user_params[:authenticate_phone]
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      first_name: session[:first_name],
+      family_name: session[:family_name],
+      ja_first_name: session[:ja_first_name],
+      ja_family_name: session[:ja_family_name],
+      birthday: '0000-00-00',
+      authenticate_phone: session[:authenticate_phone]
+    )
+    unless @user.valid?(:validates_step2)
+      flash[:notice] = @user.errors.full_messages 
+      render '/signup/step2' 
+    end
+  end
+
+  def validates_step3
+    @user = User.new
+    session[:address] = user_params[:address_attributes]
+    @address = Address.new(
+      postal_code: session[:address]['postal_code'],
+      prefecture: session[:address]['prefecture'],
+      city: session[:address]['city'],
+      street_number: session[:address]['street_number'],
+      building_name: session[:address]['building_name'],
+      delivery_phone: session[:address]['delivery_phone']
+    )
+    unless @address.valid?(:validates_step3)
+      flash[:notice] = @address.errors.full_messages
+      render '/signup/step3'
+    end
+  end
+
+  def validates_step4
+    session[:card] = user_params[:card_attributes]
+    @card = Card.new(
+      customer_id: session[:card]['customer_id'],
+      ecard_id: session[:card]['ecard_id']
+    )
+    unless @card.valid?(:validates_step4)
+      flash[:notice] = @card.errors.full_messages
+      render :step4
+    end
+  end
+
+
+
+
+
+#ストロングーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
   private
   def user_params
     params.require(:user).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   root 'items#index'
   devise_for :users
   resources :users
@@ -11,12 +12,13 @@ Rails.application.routes.draw do
   
   resources :signup do
     collection do
-      get 'step0' # 登録方法
-      get 'step1' # 個人情報入力
-      get 'step2' # 電話番号認証
-      get 'step3' # 発送情報
-      get 'step4' # 支払い情報
-      post 'step5' # 登録完了後
+      get 'step0'  # 登録方法
+      get 'step1'  # 個人情報入力
+      get 'step2'  # 電話番号認証
+      get 'step3'  # 発送情報
+      get 'step4'  # 支払い情報
+      post 'step5' # 登録
+      get  'done'  # 登録完了
     end
   end
 
