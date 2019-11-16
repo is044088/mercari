20d885f434ead6d4ce82fb44ca33cf1051f7513a
yasuna613 <yasuna.613@gmail.com>
2019-11-16 16:14:50 +0900

    add view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( item_show.css )
 Rails.application.config.assets.precompile += %w( item_delete.css )
 Rails.application.config.assets.precompile += %w( item_new.scss )
+Rails.application.config.assets.precompile += %w( *.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
