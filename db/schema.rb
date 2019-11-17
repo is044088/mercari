b82c72d306f7291d419951fd090b7cb790b4a8fe
yasuna613 <yasuna.613@gmail.com>
2019-11-17 14:49:55 +0900

    model change

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191114155402) do
-  
+ActiveRecord::Schema.define(version: 20191117054502) do
+
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",        null: false
     t.integer  "postal_code",    null: false
